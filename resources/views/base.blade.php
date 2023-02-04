3f7f2aca189e9610597aab4d5c92a7e9d2ca411d
Cleverson Franco <cleverson_reisferreira@hotmail.com>
2023-02-04 12:20:31 -0300

    feat: creating blade views base

---
+++ resources/views/base.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Alfasoft CRUD</title>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <div class="container">
+        @yield('main')
+    </div>
+</body>
+</html>

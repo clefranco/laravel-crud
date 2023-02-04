7447a3622647f4ae4ac3a46378829e81ea99cb0f
Cleverson Franco <cleverson_reisferreira@hotmail.com>
2023-02-04 12:19:03 -0300

    feat: add routes to contact crud

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\ContactController::class, 'index']);
+Route::resource('contacts', App\Http\Controllers\ContactController::class);
